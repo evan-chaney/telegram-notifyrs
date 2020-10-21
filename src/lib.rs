70e62829707591730f3d40bc1f28b51b51c0a88a
Evan Chaney <evanachaney@gmail.com>
2020-10-20 21:28:47 -0500

    Make this actually usable as a library

--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,30 @@
 use reqwest;
 use serde_json::json;
 use serde_json::{Map, Value};
-use std::env;
 use url::Url;
 
-pub fn send_message(msg: String) {
-    let token = env::var("TELEGRAM_BOT_TOKEN").expect("TELEGRAM_BOT_TOKEN not set");
-
-    let chat_id: i32 = env::var("TELEGRAM_CHAT_ID")
-        .expect("Missing TELEGRAM_CHAT_ID environment variable")
-        .parse()
-        .expect("Error parsing TELEGRAM_CHAT_ID as i32");
+pub fn send_message(
+    msg: String,
+    token: &str,
+    chat_id: i32,
+) -> Result<reqwest::blocking::Response, reqwest::Error> {
     let conn_url = Url::parse(&format!(
         "https://api.telegram.org/bot{token}/sendMessage",
         token = &token
     ))
-    .expect("Error building URL for Telegram-sendMessage");
+    .unwrap();
+    //.expect("Error building URL for Telegram-sendMessage");
 
-    let client = reqwest::blocking::Client::builder()
-        .build()
-        .expect("Error building Telegram sendMessage request client");
+    let client = reqwest::blocking::Client::builder().build().unwrap();
+    //.expect("Error building Telegram sendMessage request client");
 
     let mut request_body = Map::new();
     request_body.insert("text".to_string(), Value::String(msg));
     request_body.insert("chat_id".to_string(), json!(chat_id));
 
-    client
+    let res = client
         .post(&conn_url.to_string())
         .json(&request_body)
-        .send()
-        .expect("Error sending Telegram message");
-    println!("Telegram message sent successfully.")
-}
-
-fn main() {
-    send_message(String::from("Hello, world!"));
+        .send();
+    return res;
 }
