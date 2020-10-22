4915c0608865f7b9c0bd2cefc9ed1f5a7b31fac7
Evan Chaney <evanachaney@gmail.com>
2020-10-21 20:13:26 -0500

    Convert to ureq

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,16 @@
-use reqwest;
 use serde_json::json;
 use serde_json::{Map, Value};
-use url::Url;
+use ureq;
 
-pub fn send_message(
-    msg: String,
-    token: &str,
-    chat_id: i32,
-) -> Result<reqwest::blocking::Response, reqwest::Error> {
-    let conn_url = Url::parse(&format!(
-        "https://api.telegram.org/bot{token}/sendMessage",
-        token = &token
-    ))
-    .unwrap();
-    //.expect("Error building URL for Telegram-sendMessage");
-
-    let client = reqwest::blocking::Client::builder().build().unwrap();
-    //.expect("Error building Telegram sendMessage request client");
-
+pub fn send_message(msg: String, token: &str, chat_id: i32) -> ureq::Response {
     let mut request_body = Map::new();
     request_body.insert("text".to_string(), Value::String(msg));
     request_body.insert("chat_id".to_string(), json!(chat_id));
 
-    let res = client
-        .post(&conn_url.to_string())
-        .json(&request_body)
-        .send();
-    return res;
+    let resp = ureq::post(&format!(
+        "https://api.telegram.org/bot{token}/sendMessage",
+        token = &token
+    ))
+    .send_json(json!(request_body));
+    return resp;
 }
