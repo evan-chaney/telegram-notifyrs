a439d0a97f76d02891a68109db196c469c854198
Evan Chaney <evanachaney@gmail.com>
2020-10-22 19:58:33 -0500

    Add some light documentation

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,9 @@ use serde_json::json;
 use serde_json::{Map, Value};
 use ureq;
 
+/// Sends a Telegram message
+///
+/// Sends the supplied message to the designated chad ID, using the supplied token.
 pub fn send_message(msg: String, token: &str, chat_id: i32) -> ureq::Response {
     let mut request_body = Map::new();
     request_body.insert("text".to_string(), Value::String(msg));
